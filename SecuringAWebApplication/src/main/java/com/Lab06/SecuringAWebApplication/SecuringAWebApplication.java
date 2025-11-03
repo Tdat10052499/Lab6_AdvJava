57b6379f7432055bacf3905674089d689f2b8370
Hồ Du Tuấn Đạt <tdat.100524@gmail.com>
2025-11-03 14:25:41 +0700

    Add files via upload

---
+++ SecuringAWebApplication/src/main/java/com/Lab06/SecuringAWebApplication/SecuringAWebApplication.java
@@ -0,0 +1,13 @@
+package com.Lab06.SecuringAWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringAWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringAWebApplication.class, args);
+	}
+
+}
---
+++ SecuringAWebApplication/src/test/java/com/Lab06/SecuringAWebApplication/SecuringAWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lab06.SecuringAWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringAWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
